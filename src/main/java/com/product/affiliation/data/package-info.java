0164874ba83f0f533ea2a25734c61169f33408a7
Pankaj Pardasani <pankaj.d.p@gmail.com>
2024-02-10 15:17:15 +0000

    first commit

---
+++ src/main/java/com/product/affiliation/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.product.affiliation.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/product/affiliation/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.product.affiliation.services;
+
+import org.springframework.lang.NonNullApi;
